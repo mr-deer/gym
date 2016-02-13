fb13b676fe2dac891bbb28c50414056dc730f7a2
MogilevichDmitry <dmsmain@gmail.com>
2016-02-13 21:04:08 +0300

    v0.16
    + delay load

--- js/common.js
+++ js/common.js
@@ -4,4 +4,14 @@ $(document).on('ready', function () {
         captions: true,
         pager:false,
     });
+
+    $(function() {
+        $('#information-block .delay-load').each(function(n) {
+            $(this).delay(n*900).fadeTo(1500, 1).css('display', 'block');
+        })
+
+        $('.delay-load').each(function(n) {
+            $(this).delay(n*900).fadeTo(1500, 1);
+        })
+    })
 });
