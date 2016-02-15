719d5a3948dc2ca51cbbfe280b6a92938d221a27
MogilevichDmitry <dmsmain@gmail.com>
2016-02-15 20:54:49 +0300

    v0.21
    optimized images
    main block changed
    - delay load

--- js/common.js
+++ js/common.js
@@ -4,13 +4,4 @@ $(document).on('ready', function () {
         captions: true,
         pager:false,
     });
-
-    $(function() {
-        $('#information-block .delay-load').each(function(n) {
-            $(this).delay(n*700).fadeTo(500, 1).css('display', 'block');
-        })
-        $('.delay-load').each(function(n) {
-            $(this).delay(n*200).fadeTo(300, 1);
-        })
-    })
 });
